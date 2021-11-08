55eb475e73e765394fbf037f87562a4246faf188
moncefplastin07 <moncefplastin07@gmail.com>
2021-11-08 16:33:33 +0100

    remove not used files

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,10 +2,6 @@ import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 
-type Props = {
-  children?: ReactNode
-  title?: string
-}
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
   <div>
@@ -21,7 +17,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     </div>
     <footer>
       <hr />
-      <span>مرحبا</span>
+      <span>مرحبا 👋</span>
     </footer>
   </div>
 )
