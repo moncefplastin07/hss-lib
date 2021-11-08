fc3b5ffe1f58b876c8ade882353c897b05a589d6
moncefplastin07 <moncefplastin07@gmail.com>
2021-11-08 16:30:07 +0000

    Deploying to gh-pages from @ moncefplastin07/hss-lib@97dc47a3ab3750f225f45dcb19828f3a460484f1 🚀

--- _next/static/CRG5sGaehd6Yhq2GaiRFm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fdd7a72058a0ed41.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/about":["static/chunks/pages/about-c2234e102d1f1ead.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
