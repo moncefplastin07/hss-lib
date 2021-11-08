d81db7abfec1734908c61af677a5c09a036a75a7
Moncef Gaha <moncefplastin07@gmail.com>
2021-11-08 18:11:53 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const IndexPage = () => {
   return (
     <Layout title="الفهرس المتاح على الخط - علوم انسانية واجتماعية - شتمة بسكرة">
       <br/><br/>
-      <strong className="mx-3 bg-green-100 py-5 px-3 rounded-md text-lg">يجب ان تكون عبارة البحث اكثر من حرفين ليبدئ البحث</strong><br/><br/>
+      <strong className="xs:text-sm mx-3 bg-green-100 py-5 px-3 rounded-md text-lg">يجب ان تكون عبارة البحث اكثر من حرفين ليبدئ البحث</strong><br/><br/>
       <input type="text" id="q" placeholder="ادخل عبارة البحث ثم اضغط Enter" className="px-4 py-3 text-right w-3/6 xs:w-full" onChange={searchHandle}/>
       <p className="pt-3 pb-5">{searchResult.length > 0 ? `توجد ${searchResult.length} نتيجة بحث متطابقة` : ""}{showSearchLoading ? "جاري البحث" : ""}</p>
       <div className="grid grid-cols-3 xs:grid-cols-2 gap-6 w-3/6 m-auto ">
