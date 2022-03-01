154f089428237a75232795f20893794fd125a0cc
moncefplastin07 <moncefplastin07@gmail.com>
2022-03-01 10:04:36 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const IndexPage = () => {
   const [searchResult, setSearchResult] = useState([])
   const searchResponse = async (query: string, db = '') => {
     setSearchResultMessage("جار البحث ..")
-    return await (await fetch(`https://hss-lib.herokuapp.com/?q=${query}&dbs=${db}`)).json()
+    return await (await fetch(`http://localhost:3001/?q=${query}&dbs=${db}`)).json()
   }
   const searchHandle = async () => {
     const query = (document.getElementById("q") as HTMLInputElement).value 
@@ -36,58 +36,58 @@ const IndexPage = () => {
       <strong className="xs:text-sm bg-green-100 py-4 px-5 rounded-md text-md">يجب ان تكون عبارة البحث اكثر من حرفين ليبدئ البحث</strong><br/><br/>
       <input type="text" id="q" placeholder="ادخل عبارة البحث ثم اضغط Enter" className="px-4 py-3 text-right w-3/6 xs:w-full" onChange={searchHandle}/>
       <p className="pt-3 pb-5">{ searchResultMessage }</p>
-      <div className="grid grid-cols-3 xs:grid-cols-2 gap-6 w-3/6 m-auto ">
+      <div className="grid grid-cols-3 xs:grid-cols-2 gap-6 w-3/6 xs:w-full m-auto ">
       <span>
-          علم مكتبات
-          <input type="radio" name="db" value="li" onChange={searchHandle} />
+          <label htmlFor="li">علم مكتبات</label>
+          <input type="radio" name="db" value="li" id='li' onChange={searchHandle} />
         </span>
         <span>
-          تاريخ
-          <input type="radio" name="db" value="hs" onChange={searchHandle} />
+          <label htmlFor="hs">تاريخ</label>
+          <input type="radio" name="db" value="hs" id='hs' onChange={searchHandle} />
         </span>
         <span>
-          اتصال
-          <input type="radio" name="db" value="co" onChange={searchHandle} />
+          <label htmlFor="co">اتصال</label>
+          <input type="radio" name="db" value="co" id='co' onChange={searchHandle} />
         </span>
         <span>
-          ثقافة عامة
-          <input type="radio" name="db" value="th" onChange={searchHandle} />
+          <label htmlFor="th">ثقافة عامة</label>
+          <input type="radio" name="db" value="th" id='th' onChange={searchHandle} />
         </span>
         <span>
-          علم الاجتماع
-          <input type="radio" name="db" value="so" onChange={searchHandle}/>
+          <label htmlFor="so">علم الاجتماع</label>
+          <input type="radio" name="db" value="so" id='so' onChange={searchHandle}/>
         </span>
         <span>
-          علم النفس
-          <input type="radio" name="db" value="pc" onChange={searchHandle}/>
+          <label htmlFor="pc">علم النفس</label>
+          <input type="radio" name="db" value="pc" id="pc" onChange={searchHandle}/>
         </span>
         <span>
-          لغات اجنبية (فرنسية فقط)
-          <input type="radio" name="db" value="fl" onChange={searchHandle}/>
+          <label htmlFor="fl">لغات اجنبية (فرنسية فقط)</label>
+          <input type="radio" name="db" value="fl" id='fl' onChange={searchHandle}/>
         </span>
       </div>
      { searchResult.length > 0 ?  <div className="text-right">
         <table dir="rtl" className="m-auto">
           <thead>
             <tr className='font-mono'>
-              <th>المعرف</th>
-              <th>العنوان</th>
-              <th>الكاتب</th>
-              <th className="xs:hidden">دار النشر</th>
-              <th className="xs:hidden">سنة النشر</th>
-              <th className="xs:hidden">الدولة</th>
+              <th className="px-2 py-1.5">المعرف</th>
+              <th className="px-2 py-1.5">العنوان</th>
+              <th className="px-2 py-1.5">الكاتب</th>
+              <th className="xs:hidden px-2 py-1.5">دار النشر</th>
+              <th className="xs:hidden px-2 py-1.5">سنة النشر</th>
+              <th className="xs:hidden px-2 py-1.5">الدولة</th>
             </tr>
           </thead>
           <tbody>
             {
               searchResult.map((book:any, index)=>{
-                return <tr className={ index % 2 == 0 ? "bg-gray-200" : ''}>
-                <td>{book.ID}</td>
-                <td>{book.title}</td>
-                <td>{book.author}</td>
-                <td className="xs:hidden">{book.publisher}</td>
-                <td className="xs:hidden">{book.publishYear}</td>
-                <td className="xs:hidden">{book.country}</td>
+                return <tr className={ index % 2 == 0 ? "bg-gray-100" : ''}>
+                <td className="px-2 py-1.5 border-r border-gray-100">{book.ID}</td>
+                <td className="px-2 py-1.5 border-r border-gray-100">{book.title}</td>
+                <td className="px-2 py-1.5 border-r border-gray-100">{book.author}</td>
+                <td className="xs:hidden px-2 py-1.5 border-r border-gray-100">{book.publisher}</td>
+                <td className="xs:hidden px-2 py-1.5 border-r border-gray-100">{book.publishYear}</td>
+                <td className="xs:hidden px-2 py-1.5 border-r border-gray-100">{book.country}</td>
               </tr>
               })
             }
